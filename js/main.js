75036d80e8eb0afa30ca25655a332371f50a3dee
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2020-11-17 22:27:52 +0100

    Obdelnik

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ let trojuhelnikA = 0;
 let trojuhelnikV = 0;
 let kruhP = 0;
 let Vysledek = document.getElementById('Vysledek')
-/*document.getElementById('ctverecOBS').addEventListener('click',function(){
-    ctverecA = document.getElementById('ctverecA').value;
-
-    function obsah(a){
-        return a*a;
-    }
- document.getElementById('Vysledek').innerHTML = "<p>" + obsah(ctverecA).toFixed(2) + " cm^2 </p>"
- ctverecA = 0;
-});*/
+function obsah(a){
+    return a*a;
+}
+function obsahOBD(a,b){
+    return a*b;
+}
 document.getElementById('ctverecklik').addEventListener('click',function(){
     ctverecA = document.getElementById('ctverecOBS').value;
     Vysledek.value = obsah(ctverecA);
 })
-function obsah(a){
-    return a*a;
-}
+document.getElementById('obdelniklik').addEventListener('click',function(){
+    obdelnikA = document.getElementById('obdelnikOBS1').value;
+    obdelnikB = document.getElementById('obdelnikOBS2').value;
+    Vysledek.value = obsahOBD(obdelnikA,obdelnikB);
+})
